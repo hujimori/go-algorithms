89aa957dcf30cd1ed7e0d6443972472e680ed416
hujimori <hujimori16@gmail.com>
2019-07-11 14:02:31 +0900

    スタック実装

---
+++ stack/stack.go
@@ -0,0 +1,41 @@
+package stack
+
+type StackData struct {
+	data interface{}
+	next *StackData
+}
+
+type Stack struct {
+	sp  *StackData
+	top uint64
+}
+
+func New() *Stack {
+	return &Stack{top: 0}
+}
+
+func (s *Stack) Push(data interface{}) {
+	s.sp = &StackData{data: data, next: s.sp}
+	s.top++
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.top > 0 {
+		item := s.sp.data
+		s.sp = s.sp.next
+		s.top--
+		return item
+	}
+	return nil
+}
+
+func (s *Stack) Peek() interface{} {
+	if s.top > 0 {
+		return s.sp.data
+	}
+	return nil
+}
+
+func (s *Stack) Size() uint64 {
+	return s.top
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,42 @@
+package stack
+
+import (
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	tests := []int{
+		1,
+		2,
+		3,
+		4,
+		5,
+	}
+
+	s := New()
+
+	for _, tt := range tests {
+		s.Push(tt)
+
+		if tt != s.Peek() {
+			t.Errorf("push(%d) is Error. got=%d", tt, s.Peek())
+		}
+
+		t.Log(s.Peek())
+
+	}
+
+	if int(s.Size()) != len(tests) {
+		t.Errorf("Size() is Error. got=%d. expected=%d", s.Size(), len(tests))
+	}
+
+	for i := len(tests) - 1; i >= 0; i-- {
+		x := s.Pop()
+		t.Log(x)
+
+		if x != tests[i] {
+			t.Errorf("pop() is Error. got=%d. expected=%d", x, tests[i])
+		}
+	}
+
+}
