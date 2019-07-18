32d2332e05c8b6de42687df482a82211096df23a
hujimori <hujimori16@gmail.com>
2019-07-18 10:35:54 +0900

    二分探索

---
+++ binarysearch/binarysearch.go
@@ -0,0 +1,31 @@
+package binarysearch
+
+func LowerBound(t []int, k int) int {
+	lb := -1
+	ub := len(t)
+	for ub-lb > 1 {
+		mid := (lb + ub) / 2
+
+		if t[mid] >= k {
+			ub = mid
+		} else {
+			lb = mid
+		}
+	}
+	return ub
+}
+
+func UpperBound(t []int, k int) int {
+	lb := -1
+	ub := len(t)
+	for ub-lb > 1 {
+		mid := (lb + ub) / 2
+
+		if t[mid] <= k {
+			lb = mid
+		} else {
+			ub = mid
+		}
+	}
+	return ub
+}
